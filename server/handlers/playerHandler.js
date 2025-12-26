8fdaf9a234980f408078fd75ba56ae3daa875915
unknown <nurbekrasulov71@gmail.com>
2025-12-26 16:09:02 +0500

    fix43

--- server/handlers/playerHandler.js
+++ server/handlers/playerHandler.js
@@ -1,17 +1,24 @@
+const players = new Map();
+
 const handlePlayerConnection = (io, socket) => {
   console.log('A user connected:', socket.id);
 
   // Broadcast to all other clients that a new player joined
   socket.broadcast.emit('playerJoined', socket.id);
 
-  // Send current players to the new player
-  const currentPlayers = Object.keys(io.sockets.sockets).filter(id => id !== socket.id);
-  currentPlayers.forEach(playerId => {
-    socket.emit('playerJoined', playerId);
+  // Send current players' positions to the new player
+  players.forEach((data, playerId) => {
+    socket.emit('playerUpdate', {
+      id: playerId,
+      position: data.position,
+      rotation: data.rotation,
+    });
   });
 
   // Handle position updates from clients
   socket.on('updatePosition', (data) => {
+    // Update stored position
+    players.set(socket.id, { position: data.position, rotation: data.rotation });
     // Broadcast the position update to all other clients
     socket.broadcast.emit('playerUpdate', {
       id: socket.id,
@@ -23,6 +30,7 @@ const handlePlayerConnection = (io, socket) => {
   // Handle disconnection
   socket.on('disconnect', () => {
     console.log('User disconnected:', socket.id);
+    players.delete(socket.id);
     // Broadcast to all other clients that a player left
     socket.broadcast.emit('playerDisconnected', socket.id);
   });
